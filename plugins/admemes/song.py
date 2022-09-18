fccfe300a2620be40d1a50318567a1795d7e416b
Anurag Maheshwari <41854065+8769ANURAG@users.noreply.github.com>
2022-09-18 21:11:12 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [ᎯℕUℛᎯᎶ](https://youtube.com/channel/UCqts9WhhlioK3RB9XQQzoAg)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [muѕíc вσч](https://t.me/AnuragB8_bot)**'
+        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [ᎯℕUℛᎯᎶ](https://youtube.com/channel/UCqts9WhhlioK3RB9XQQzoAg)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [muѕíc вσч](https://t.me/BetterFilters_Ro_Bot)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
