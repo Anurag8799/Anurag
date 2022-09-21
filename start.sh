7434c3a9beada425b0ca5f750aed82e337313b8f
Anurag Maheshwari <41854065+8769ANURAG@users.noreply.github.com>
2022-09-21 21:48:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/8769/Anurag.git /Anurag
+  git clone https://github.com/8769ANURAG/Anurag.git /Anurag
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Amurag
+  git clone $UPSTREAM_REPO /Anurag
 fi
 cd /Anurag
 pip3 install -U -r requirements.txt
