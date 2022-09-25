04a9efdce7ed7c2e1b248f33f905f17ebf530c95
Anurag8799 <114276044+Anurag8799@users.noreply.github.com>
2022-09-26 02:52:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/8769ANURAG/Anurag.git /Anurag
+  git clone https://github.com/Anurag8799/Anurag.git /Anurag
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Anurag
