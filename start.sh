571fc0dfa9a7fcd1f28652b2989a8e8501115217
Anurag8799 <114276044+Anurag8799@users.noreply.github.com>
2022-09-26 03:06:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Anurag8799/Anurag.git /Anurag
+  git clone https://github.com/Anurag8799/Anurag /Anurag
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Anurag
 fi
 cd /Anurag
 pip3 install -U -r requirements.txt
-echo "Starting ᎯℕUℛᎯᎶ....🔥"
+echo "Starting Bot...."
 python3 bot.py
